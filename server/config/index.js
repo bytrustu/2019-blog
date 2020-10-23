770060dcb5b48d29bb9470f3bfd39ef4350760de
bytrustu <bytrustu@gmail.com>
2020-10-24 06:57:27 +0900

    fixed

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config({ path: path.join(__dirname, '../.env') })
+dotenv.config()
 
 module.exports = {
     MONGO_URI: process.env.MONGO_URI,
